941644e1ad91f8375b586442e0dfad050f950d41
Ian Travers <mr.itpe@gmail.com>
2021-04-01 11:29:52 +0300

    Login feature

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+<x-layouts.app>
+    <h2>Register</h2>
+
+    <form action="/login" method="post">
+        @csrf
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}" >
+        </div>
+        @error('email')<p>{{ $message }}</p>@enderror
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" >
+        </div>
+        @error('password')<p>{{ $message }}</p>@enderror
+
+        <div>
+            <input type="checkbox" id="remember" name="remember">
+            <label for="remember">Remember Me</label>
+        </div>
+
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</x-layouts.app>
